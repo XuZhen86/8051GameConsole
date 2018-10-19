53dcfde6f16241c949cbe9e9121b6fc3cf304a0e
Zhen Xu <curt_xu@me.com>
2018-10-19 09:24:14 -0400

    Refactor ADC

--- Sources/ADC/ADC.c
+++ Sources/ADC/ADC.c
@@ -1,9 +1,11 @@
+#include"Sources/Main/STC15W4K48S4.h"
+
 #include"Sources/ADC/ADC.h"
 
 static unsigned char adcSpeed=0;
 
 void adc_initialize(unsigned char speed){
-    CLK_DIV|=0x20;  // ADC_RES[1,0]|ADC_RESL[7,0]
+    CLK_DIV|=0x20;
     ADC_CONTR=0x80;
     adcSpeed=(speed&0x03)<<5;
 }
@@ -12,9 +14,9 @@ unsigned int adc_get(unsigned char channel){
     unsigned int result;
 
     P1ASF|=(1<<channel);
-    ADC_CONTR=0x88|adcSpeed|(channel%8);
-    while(!(ADC_CONTR&0x10));
-    P1ASF=0;
+    ADC_CONTR=0x88|adcSpeed|channel%8;
+    while((ADC_CONTR&0x10)==0);
+    P1ASF=0x00;
 
     result=ADC_RES;
     result<<=8;
--- Sources/ADC/ADC.h
+++ Sources/ADC/ADC.h
@@ -1,8 +1,6 @@
 #ifndef ADC_H_
 #define ADC_H_
 
-#include<Sources/Main/STC15W4K48S4.h>
-
 void adc_initialize(unsigned char speed);
 unsigned int adc_get(unsigned char channel);
 
