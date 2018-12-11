ce09908d7f3a8296f6f3470bc1c46c4b7efd3712
Zhen Xu <curt_xu@me.com>
2018-12-10 20:15:48 -0500

    Implement an error check in FarMemBlock using XOR and SUM

--- Sources/FarMem/FarMem.c
+++ Sources/FarMem/FarMem.c
@@ -4,7 +4,6 @@
 #include"./FarMemBlock.h"
 #include"./FarMemConfig.h"
 #include"../XRAM/XRAM.h"
-#include"../Delay/Delay.h"
 
 FarMemBlock far head _at_ 0x020000;
 unsigned int data
@@ -16,6 +15,8 @@ void farMem_Initialize(){
     head.size=0x10000-sizeof(FarMemBlock);
     head.attr=0x00;
     head.next=NULL;
+    head.pad[0]=calculateFarMemBlockPad0(&head);
+    head.pad[1]=calculateFarMemBlockPad1(&head);
 
     usedSpace=0;
     fragmentedFreeByte=0;
@@ -26,15 +27,24 @@ void *farMalloc(unsigned int size){
     FarMemBlock *p,*np;
 
     for(p=&head;p!=NULL;p=p->next){
+        if(!verifyFarMemBlock(p)){
+            printf("[farMalloc() Memory Corruption p=0x%0x]\n",(unsigned int)p);
+            while(1);
+        }
+
         if(p->size>=size+sizeof(FarMemBlock)&&p->attr==0x00){
             np=(void *)p+sizeof(FarMemBlock)+size;
             np->attr=0x00;
             np->next=p->next;
             np->size=p->size-size-sizeof(FarMemBlock);
+            np->pad[0]=calculateFarMemBlockPad0(np);
+            np->pad[1]=calculateFarMemBlockPad1(np);
 
             p->attr=0x01;
             p->next=np;
             p->size=size;
+            p->pad[0]=calculateFarMemBlockPad0(p);
+            p->pad[1]=calculateFarMemBlockPad1(p);
 
             usedSpace+=(size+sizeof(FarMemBlock));
             printf("[farMalloc() usedSpace=%u]\n",usedSpace);
@@ -58,9 +68,14 @@ void farFree(void *ptr){
 
     if(ptr==NULL){
         return;
+    }else if(!verifyFarMemBlock(p)){
+        printf("[farFree() Memory Corruption p=0x%0x]\n",(unsigned int)p);
+        while(1);
     }
 
     p->attr=0x00;
+    p->pad[0]=calculateFarMemBlockPad0(p);
+    p->pad[1]=calculateFarMemBlockPad1(p);
     usedSpace-=(p->size+sizeof(FarMemBlock));
 
     fragmentedFreeByte+=p->size;
@@ -81,13 +96,61 @@ static void defragFreeBlock(){
     // printf("[defragFreeBlock()]");
 
     while(p!=NULL){
+        if(!verifyFarMemBlock(p)){
+            printf("[defragFreeBlock() Memory Corruption p=0x%0x]\n",(unsigned int)p);
+            while(1);
+        }
+
         if(p->attr==0x00&&p->next!=NULL&&p->next->attr==0x00){
             p->size+=sizeof(FarMemBlock)+p->next->size;
             p->next=p->next->next;
+            p->pad[0]=calculateFarMemBlockPad0(p);
+            p->pad[1]=calculateFarMemBlockPad1(p);
         }else{
             p=p->next;
         }
     }
+}
+
+static bit verifyFarMemBlock(void *ptr){
+    FarMemBlock *p=ptr;
+
+    if(calculateFarMemBlockPad0(ptr)!=p->pad[0]){
+        return 0;
+    }
+    if(calculateFarMemBlockPad1(ptr)!=p->pad[1]){
+        return 0;
+    }
+
+    return 1;
+}
+
+static unsigned char calculateFarMemBlockPad0(void *ptr){
+    unsigned char *bytes=ptr;
+    unsigned char pad;
+    unsigned char i;
+
+    pad=0;
+    for(i=0;i<6;i++){
+        pad^=bytes[i];
+    }
+
+    // printf("[pad0=0x%0x]\n",(unsigned int)pad);
+    return pad;
+}
+
+static unsigned char calculateFarMemBlockPad1(void *ptr){
+    unsigned char *bytes=ptr;
+    unsigned char pad;
+    unsigned char i;
+
+    pad=0;
+    for(i=0;i<6;i++){
+        pad+=bytes[i];
+    }
+
+    // printf("[pad1=0x%0x]\n",(unsigned int)pad);
+    return pad;
 }
 
 void farDump(){
--- Sources/FarMem/FarMem.h
+++ Sources/FarMem/FarMem.h
@@ -7,6 +7,9 @@ void *farCalloc(unsigned int num,unsigned int size);
 void farFree(void *ptr);
 
 static void defragFreeBlock();
+static bit verifyFarMemBlock(void *ptr);
+static unsigned char calculateFarMemBlockPad0(void *ptr);
+static unsigned char calculateFarMemBlockPad1(void *ptr);
 
 void farDump();
 
